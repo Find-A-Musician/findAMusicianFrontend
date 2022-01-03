012a25ddafcf6e396c32de91e14a49201482d117
RomainGuarinoni <romain.guar91@gmail.com>
2022-01-03 16:09:23 +0100

    feat(index): add redirection in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
 import Login from './login';
+import Dashboard from './dashboard';
+import { useAuth } from '../context/AuthContext';
 
 const Home: NextPage = () => {
+  const { isAuthenticated } = useAuth();
+
+  if (isAuthenticated()) {
+    return <Dashboard />;
+  }
+
   return <Login />;
 };
 
