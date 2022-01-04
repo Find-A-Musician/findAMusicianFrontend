8c173f74adcf6f9206f18b87c6bbca0f241dec46
RomainGuarinoni <romain.guar91@gmail.com>
2022-01-04 15:14:13 +0100

    feat(tailwind): add colors of the app

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   ],
 
   theme: {
+    colors: {
+      red: '#af1026',
+      purple: '#58335e',
+      error: '#e53935',
+      success: '#388e3c',
+      blue: '#26c6da',
+      yellow: '#c0ca33',
+      pink: '#d81b60',
+      orange: '#fb8c00',
+    },
     extend: {},
   },
   plugins: [],
