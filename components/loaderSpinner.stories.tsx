2929b408e35c823ab5aba1fe83f6960d76435ab7
RomainGuarinoni <romain.guar91@gmail.com>
2022-01-05 21:14:47 +0100

    feat(components): loading spinner

---
+++ components/loaderSpinner.stories.tsx
@@ -0,0 +1,15 @@
+import loaderSpinner, { SPINNER_SIZE } from './loaderSpinner';
+import { Meta } from '@storybook/react';
+
+export default {
+  title: 'Loader/Circle',
+  component: loaderSpinner,
+  argTypes: {
+    size: {
+      options: SPINNER_SIZE,
+      control: { type: 'select' },
+    },
+  },
+} as Meta;
+
+export { loaderSpinner };
---
+++ components/loaderSpinner.tsx
@@ -0,0 +1,30 @@
+export const SPINNER_SIZE = ['xs', 'sm', 'lg', 'xl', '2xl'] as const;
+
+type Size = typeof SPINNER_SIZE[number];
+
+export default function loaderSpinner({ size }: { size: Size }): JSX.Element {
+  return (
+    <div
+      className={`${getSize(
+        size,
+      )} rounded-full border-4 border-gray-200 border-t-red-800 animate-spin`}
+    >
+      <div></div>
+    </div>
+  );
+}
+
+function getSize(size: Size) {
+  switch (size) {
+    case 'xs':
+      return 'w-5 h-5';
+    case 'sm':
+      return 'w-8 h-8';
+    case 'lg':
+      return 'w-10 h-10';
+    case 'xl':
+      return 'w-20 h-20';
+    case '2xl':
+      return 'w-52 h-52';
+  }
+}
