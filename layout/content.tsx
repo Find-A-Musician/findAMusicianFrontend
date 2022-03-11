34836310a231428945b355fdc6800229607a9ac1
Thomas Bernard <tbernard2000.retonfey@gmail.com>
2022-03-11 18:54:59 +0100

    fix: mistakes were made

--- layout/content.tsx
+++ layout/content.tsx
@@ -7,7 +7,7 @@ type Props = {
 export default function ContentLayout({ Header, children }: Props) {
   return (
     <div className="flex flex-col pb-10">
-      <div className="sticky top-0">{Header}</div>
+      <div className="sticky top-0 z-10">{Header}</div>
       <div className="flex flex-col flex-grow gap-8">{children}</div>
     </div>
   );
