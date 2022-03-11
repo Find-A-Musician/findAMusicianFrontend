4989e98307bb80273429784b657ce9964b19163b
Alexandre Lam <alexandrelam@outlook.com>
2022-03-11 18:36:40 +0100

    404 :red_square: (#65)

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,21 +1,25 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Custom404() {
   return (
-    <div>
-      <h1>Page introuvable</h1>
-      <ul>
-        <li>
-          <Link href="/">
-            <a>Home Page</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/login">
-            <a>Login</a>
-          </Link>
-        </li>
-      </ul>
+    <div className="flex flex-col justify-center items-center h-full gap-4">
+      <h2 className="text-5xl font-bold text-red-500 mb-8">
+        404 something went wrong
+      </h2>
+      <Image
+        src="/images/404.jpg"
+        alt="404"
+        width={500}
+        height={(500 * 813) / 1006}
+        className="rounded"
+      />
+      <span className="italic text-xl text-gray-500">
+        Hey ! Qu'est-ce que tu fais là ? Retourne sur{' '}
+        <Link href="/musicians" passHref>
+          <a className="text-blue-500 hover:underline">une page qui existe !</a>
+        </Link>
+      </span>
     </div>
   );
 }
