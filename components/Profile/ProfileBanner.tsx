b4c0acdd9f126b550018b4869dfb5ad01a0939b3
Alexandre Lam <alexandrelam@outlook.com>
2022-03-14 13:28:36 +0100

    feat: is looking for group tag on profil page

--- components/Profile/ProfileBanner.tsx
+++ components/Profile/ProfileBanner.tsx
@@ -4,6 +4,7 @@ import { capitalize } from '../../utils/string';
 import Toggle from '../Toggle';
 import { useAxios } from '../../context/AxiosContext';
 import { mutate } from 'swr';
+import MyProfile from '../../pages/profile';
 
 type Props = {
   profil: Musician;
@@ -41,9 +42,16 @@ export function ProfileBanner({ profil, isMyProfile = false, groups }: Props) {
         <div className="lg:flex-none w-52 h-52 border-2 border-white ml-10 -mt-20 rounded-3xl md:rounded-full bg-red-100"></div>
         <div className="flex flex-wrap gap-4 items-center w-full lg:-mt-10">
           <div className="flex-grow">
-            <h2 className="text-2xl font-bold">
-              {profil.givenName} {profil.familyName}
-            </h2>
+            <div className="flex items-center gap-3">
+              <h2 className="text-2xl font-bold">
+                {profil.givenName} {profil.familyName}
+              </h2>
+              {profil.isLookingForGroups && !isMyProfile && (
+                <span className="text-sm px-2 py-0.5 text-gray-500 border rounded">
+                  Recherche un groupe
+                </span>
+              )}
+            </div>
             <span className="text-lg text-gray-500">
               {groups?.length
                 ? groups?.map((group) => capitalize(group.name)).join(', ')
