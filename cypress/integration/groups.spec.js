697c72df7aa14acefbe39fc02fa053c1e860a855
Alexandre Lam <alexandrelam@outlook.com>
2022-05-15 13:11:10 +0200

    test: remove create

--- cypress/integration/groups.spec.js
+++ cypress/integration/groups.spec.js
@@ -29,22 +29,4 @@ describe('groups page', () => {
     cy.contains('Créer un groupe');
     cy.get('[data-cy=group-list]').children().should('have.length', 5);
   });
-
-  it('create a group', () => {
-    /* ==== Generated with Cypress Studio ==== */
-    cy.get('.gap-3 > .px-4').click();
-    cy.get('#name').clear();
-    cy.get('#name').type('test');
-    cy.get(':nth-child(2) > .css-b62m3t-container > .css-1s2u09g-control > .css-319lph-ValueContainer > .css-6j8wv5-Input').click();
-    cy.get('#react-select-3-option-0').click();
-    cy.get('.bg-gray-50').click();
-    cy.get(':nth-child(4) > .css-b62m3t-container > .css-1s2u09g-control > .css-319lph-ValueContainer > .css-6j8wv5-Input').click();
-    cy.get('#react-select-5-option-1').click();
-    cy.get('.css-g1d714-ValueContainer > .css-6j8wv5-Input').click();
-    cy.get('#react-select-5-option-0').click();
-    cy.get(':nth-child(5) > .css-b62m3t-container > .css-1s2u09g-control > .css-319lph-ValueContainer > .css-6j8wv5-Input').click();
-    cy.get('#react-select-7-option-0').click();
-    cy.get('.py-2\\.5').click();
-    /* ==== End Cypress Studio ==== */
-  });
 });
